fa3edfe7756f8976f59a97a1ceaf428ebf737fc0
Yuxing Chen <yxchen2828@gmail.com>
2017-03-16 12:41:32 -0700

    Create conf.py

---
+++ Project/conf.py
@@ -0,0 +1,12 @@
+# Google Places API key                                                         
+API_KEY="your API_KEY"
+
+# TCP port numbers for server instances                                         
+# Please use the port numbers allocated by the TA.                              
+PORT_NUM = {
+    'Alford': 12090,
+    'Ball': 12091,
+    'Hamilton': 12092,
+    'Holiday': 12093,
+    'Welsh': 12094
+}
